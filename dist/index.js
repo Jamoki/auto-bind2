375c33eec4b7a3a3c1fb3c8594605c2c626175af
John Lyon-Smith <john@lyon-smith.org>
2017-10-15 17:01:29 -0700

    Version 1.0.3-20171016.0

--- dist/index.js
+++ dist/index.js
@@ -1,37 +1,37 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-	value: true
+  value: true
 });
 exports.reactAutoBind = reactAutoBind;
 function autoBind(obj, filter) {
-	filter = filter || function () {
-		return true;
-	};
+  filter = filter || function () {
+    return true;
+  };
 
-	Object.getOwnPropertyNames(obj.constructor.prototype).forEach(function (name) {
-		var val = obj[name];
+  Object.getOwnPropertyNames(obj.constructor.prototype).forEach(function (name) {
+    var val = obj[name];
 
-		if (name !== 'constructor' && typeof val === 'function' && filter(name)) {
-			obj[name] = val.bind(obj);
-		}
-	});
+    if (name !== 'constructor' && typeof val === 'function' && filter(name)) {
+      obj[name] = val.bind(obj);
+    }
+  });
 
-	return obj;
+  return obj;
 }
 
 exports.autoBind = autoBind;
 exports.default = autoBind;
 var isReactMethod = exports.isReactMethod = function isReactMethod(name) {
-	'render', 'componentWillReceiveProps', 'componentDidMount', 'componentDidUpdate', 'shouldComponentUpdate', 'componentWillUnmount', 'componentWillUpdate', 'forceUpdate', 'componentWillMount';
+  return ['render', 'componentWillReceiveProps', 'componentDidMount', 'componentDidUpdate', 'shouldComponentUpdate', 'componentWillUnmount', 'componentWillUpdate', 'forceUpdate', 'componentWillMount'].includes(name);
 };
 
 function reactAutoBind(obj, filter) {
-	filter = filter || function () {
-		return true;
-	};
-	return autoBind(obj, function (name) {
-		return !isReactMethod(name) && filter(name);
-	});
+  filter = filter || function () {
+    return true;
+  };
+  return autoBind(obj, function (name) {
+    return !isReactMethod(name) && filter(name);
+  });
 }
 //# sourceMappingURL=index.js.map
